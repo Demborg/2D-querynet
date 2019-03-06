dbc8910f6c7099f53ddbe0f2e08325d1dc9741b6
Axel Demborg <axel.demborg@gmail.com>
2019-03-06 17:13:05 +0100

    adds auto decoder part

--- querynet.py
+++ querynet.py
@@ -7,7 +7,6 @@ from keras.utils import Sequence
 from keras.layers import Dense, Embedding, Input, Concatenate, Reshape
 from keras.models import Model, load_model
 from sklearn.decomposition import PCA
-from sklearn.manifold import TSNE
 
 
 def load_batch(path: str):
@@ -15,6 +14,7 @@ def load_batch(path: str):
     image_data = np.reshape(data_dict[b'data'], [-1, 3, 32, 32])
     image_data = np.moveaxis(image_data, 1, -1)
     return image_data / 255, data_dict[b'labels']
+
 
 class DataGenerator(Sequence):
     def __init__(self, data_array, batch_size=32**2):
@@ -25,6 +25,7 @@ class DataGenerator(Sequence):
         return self.data_array.shape[0]
 
     def __getitem__(self, idx):
+        np.random.seed(idx)
         query_points_x = np.random.randint(0, self.data_array.shape[1], self.batch_size)
         query_points_y = np.random.randint(0, self.data_array.shape[2], self.batch_size)
         color_values = self.data_array[idx, 
@@ -39,6 +40,7 @@ class DataGenerator(Sequence):
         return ({'coordinates': query_coordinates,
                  'idx': np.repeat(idx, self.batch_size)},
                 color_values)
+
 
 def create_representation(num_images: int, embedding_dim: int):
     idx = Input(shape=(None, ), name='idx')
@@ -62,17 +64,17 @@ def create_render_network(embedding_dim: int):
     x = Dense(64, activation='relu')(x)
     x = Dense(3, activation='sigmoid')(x)
 
-    return Model(inputs = [coordinates, latent_vec],
-                 outputs = x)
+    return Model(inputs=[coordinates, latent_vec],
+                 outputs=x)
 
-def make_training_model(num_images: int, embedding_dim: int):
+
+def make_models(num_images: int, embedding_dim: int):
     rep = create_representation(num_images, embedding_dim)
     render = create_render_network(embedding_dim)
     coordinates = Input(shape=[2], name='coordinates')
     
     output = render([coordinates, rep.output])
-    return Model(inputs=[coordinates, rep.input], outputs=output)
-
+    return Model(inputs=[coordinates, rep.input], outputs=output), render
 
 
 def image_from_idx(model, idx, image_size=(32, 32)):
@@ -86,6 +88,7 @@ def image_from_idx(model, idx, image_size=(32, 32)):
     for c, v in zip(coordinates, y):
         img[c[0],c[1] :] = v      
     return img
+
 
 def parse_args():
     parser = ArgumentParser()
@@ -109,6 +112,7 @@ def parse_args():
     parser.add_argument('--steps_per_epoch', default=None, type=int)
     return parser.parse_args()
 
+
 def encoder_plot(model, data):
     for i in range(10):
         plt.figure('GT')
@@ -118,6 +122,7 @@ def encoder_plot(model, data):
         plt.subplot(5, 2, i + 1)
         plt.imshow(data[i, ...])
 
+
 def embedding_plot(embeddings, labels):
     pca = PCA(n_components=2)
     plt.figure("PCA")
@@ -126,21 +131,40 @@ def embedding_plot(embeddings, labels):
       % str(pca.explained_variance_ratio_))
     plt.scatter(X[:,0], X[:,1], c=labels, cmap='tab10', alpha=0.3)
 
-    #plt.figure("t-sne")
-    #X_tsne = TSNE(n_components=2).fit_transform(embeddings)
-    #plt.scatter(X_tsne[:,0], X_tsne[:,1], c=labels, cmap='tab10')
-    
+
+def auto_decode(data: np.ndarray, model: Model, dim, samples: int = 1024):
+    generator = DataGenerator(data, samples)
+
+    # freeze the representation network
+    for layer in model.layers:
+        layer.trainable = False
+
+    # add a new embedding layer
+    rep = create_representation(len(data), dim)
+    coordinates = Input(shape=[2], name='coordinates')
+
+    output = model([coordinates, rep.output])
+    full = Model(inputs=[coordinates, rep.input], outputs=output)
+    full.compile('adam', loss='mean_squared_error')
+    full.fit_generator(generator, epochs=10, steps_per_epoch=10)
+    plt.figure('Auto decoded')
+    plt.subplot(211)
+    plt.imshow(image_from_idx(full, 0, image_size=(512, 512)))
+    plt.subplot(212)
+    plt.imshow(data[0])
+
 
 def main():
     args = parse_args()
-    data, labels  = load_batch(args.train_data)
+    data, labels = load_batch(args.train_data)
+    t_data, t_labels = load_batch(args.test_data)
 
     if args.load:
         model = load_model(args.model_path)
     else:
-        train_generator = DataGenerator(data[:args.limit_images,:])
+        train_generator = DataGenerator(data[:args.limit_images, :])
 
-        model = make_training_model(len(train_generator), 30)
+        model, render = make_models(len(train_generator), 30)
         model.compile('adam', loss='mean_squared_error')
         model.fit_generator(train_generator,
                             epochs=args.epochs,
@@ -154,6 +178,7 @@ def main():
             labels[:args.limit_images])
     plt.figure('super sample')
     plt.imshow(image_from_idx(model, 0, image_size=(512, 512)))
+    auto_decode(data[[0], ...], render, 30, samples=100)
     plt.show()
 
 
